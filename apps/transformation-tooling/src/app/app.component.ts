6387cec64c007be6cfba0283d353644cfb48dc13
Ignacio Fontalvo <ignacioa@backbase.com>
2022-07-23 10:24:13 +0200

    initial commit

---
+++ apps/transformation-tooling/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'na-transformation-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'transformation-tooling';
+}
