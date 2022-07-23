e282f64bc38a2c0526336096001e0823e9c39c24
Ignacio Fontalvo <ignacioa@backbase.com>
2022-07-23 22:31:32 +0200

    start making progress on the range selection component

--- libs/self-diagnosis/src/lib/ui-range-selection/ui-range-selection.component.ts
+++ libs/self-diagnosis/src/lib/ui-range-selection/ui-range-selection.component.ts
@@ -3,7 +3,54 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ui-range-selection',
-  template: `<div>Range Selection component over here</div>`,
+  template: `<div class="w-full flex border h-12 border-gray-400 rounded-lg">
+    <div
+      class="flex-1 flex justify-center items-center border-r border-gray-400"
+    >
+      1
+    </div>
+    <div
+      class="flex-1 flex justify-center items-center border-r border-gray-400"
+    >
+      2
+    </div>
+    <div
+      class="flex-1 flex justify-center items-center border-r border-gray-400"
+    >
+      3
+    </div>
+    <div
+      class="flex-1 flex justify-center items-center border-r border-gray-400"
+    >
+      4
+    </div>
+    <div
+      class="flex-1 flex justify-center items-center border-r border-gray-400"
+    >
+      5
+    </div>
+    <div
+      class="flex-1 flex justify-center items-center border-r border-gray-400"
+    >
+      6
+    </div>
+    <div
+      class="flex-1 flex justify-center items-center border-r border-gray-400"
+    >
+      7
+    </div>
+    <div
+      class="flex-1 flex justify-center items-center border-r border-gray-400"
+    >
+      8
+    </div>
+    <div
+      class="flex-1 flex justify-center items-center border-r border-gray-400"
+    >
+      9
+    </div>
+    <div class="flex-1 flex justify-center items-center">10</div>
+  </div>`,
   styles: [],
 })
 export class UiRangeSelectionComponent {}
